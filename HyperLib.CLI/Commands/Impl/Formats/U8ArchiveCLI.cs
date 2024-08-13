b9ee07249e1e7247e33caa77c38f9de6605c2e33
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-08-13 06:42:28 +0100

    U8Archive stuff that's just sitting here

---
+++ HyperLib.CLI/Commands/Impl/Formats/U8ArchiveCLI.cs
@@ -0,0 +1,52 @@
+﻿using HyperLib.Formats;
+using System.IO.Compression;
+
+namespace HyperLib.CLI.Commands.Impl.Formats
+{
+    [Command("U8Archive", "arc", [typeof(string), typeof(string)], "Nintendo U8 (*.arc)",
+    [
+        "[\"file\"|\"directory\"] [opt: \"destination\"]",
+        "[opt: /Uncompressed] /SonicNext [\"directory\"] [opt: \"destination\"]",
+        "/List [\"file\"]"
+    ])]
+    public class U8ArchiveCLI : ICommand
+    {
+        public void Execute(List<Command> in_commands, Command in_command)
+        {
+            var args = CommandHelper.GetCommandArguments(in_command.Inputs);
+            var isUncompressed = false;
+
+            foreach (var arg in args)
+            {
+                var ioPaths = CommandHelper.GetInputOutputPaths(arg.Value);
+
+                switch (arg.Key)
+                {
+                    case "/":
+                        CommandHelper.HandleArchiveType<U8Archive>(ioPaths);
+                        break;
+
+                    case "/List":
+                    {
+                        // TODO
+                        break;
+                    }
+
+                    case "/Uncompressed":
+                        isUncompressed = true;
+                        break;
+
+                    case "/SonicNext":
+                    {
+                        if (isUncompressed)
+                            U8Archive.CompressionLevel = CompressionLevel.NoCompression;
+
+                        CommandHelper.HandleArchiveType<U8Archive>(ioPaths);
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- HyperLib/Formats/U8Archive.cs
+++ HyperLib/Formats/U8Archive.cs
@@ -1,5 +1,6 @@
-﻿using AuroraLib.Core;
+﻿using HyperLib.IO.Compression;
 using HyperLib.IO.Extensions;
+using System.IO.Compression;
 
 namespace HyperLib.Formats
 {
@@ -24,9 +25,12 @@ namespace HyperLib.Formats
 
         public bool IsIndexOnly { get; set; } = true;
 
+        // TODO: make these members not static...
         public static bool IsSonicNextArchive { get; set; } = false;
 
-        public List<Info> Nodes { get; set; } = [];
+        public static CompressionLevel CompressionLevel { get; set; } = IsSonicNextArchive ? CompressionLevel.Optimal : CompressionLevel.NoCompression;
+
+        public ArchiveDirectory Root { get; set; } = new();
 
         public U8Archive() { }
 
@@ -46,15 +50,74 @@ namespace HyperLib.Formats
             var reader = new BinaryObjectReader(in_stream, StreamOwnership.Retain, Endianness.Big, Encoding.ASCII);
             var header = reader.ReadObject<Header>();
 
-            var root = reader.ReadObject<Info>();
+            var root = reader.ReadObject<Node>();
 
             if (root.Type != ENodeType.Directory)
                 throw new InvalidCastException("The root node is not a directory.");
 
-            Nodes.Add(root);
+            Root = new ArchiveDirectory(root, true);
 
-            for (int i = 0; i < root.DataSize - 1; i++)
-                Nodes.Add(reader.ReadObject<Info>());
+            var stringTableOffset = header.NodeTableOffset + (root.DataSize * Node.SizeOf);
+            var nodes = reader.ReadArrayAtOffset<Node>(header.NodeTableOffset, (int)root.DataSize);
+
+            ParseNodes(0, new ArchiveDirectory() { Data = Root.Data }, true);
+
+            uint ParseNodes(uint in_nodeIndex, ArchiveDirectory in_directory, bool in_isRoot = false)
+            {
+                ref var node = ref nodes[in_nodeIndex];
+
+                reader.JumpTo(stringTableOffset + node.NameOffset);
+                string name = reader.ReadString(StringBinaryFormat.NullTerminated);
+
+                if (!in_isRoot)
+                {
+                    if (string.IsNullOrEmpty(name))
+                        return node.DataSize;
+                }
+
+                switch (node.Type)
+                {
+                    case ENodeType.Directory:
+                    {
+                        var directory = new ArchiveDirectory
+                        {
+                            Name = name,
+                            Parent = in_directory
+                        };
+
+                        in_directory.Data.Add(directory);
+
+                        if (in_isRoot)
+                            Root = directory;
+
+                        in_directory = directory;
+
+                        uint childNodeIndex = ++in_nodeIndex;
+
+                        while (childNodeIndex < node.DataSize)
+                            childNodeIndex = ParseNodes(childNodeIndex, in_directory);
+
+                        return node.DataSize;
+                    }
+
+                    case ENodeType.File:
+                    {
+                        var file = new ArchiveFile()
+                        {
+                            Name = name,
+                            Parent = in_directory,
+                            UncompressedDataSize = node.UncompressedDataSize
+                        };
+
+                        in_directory.Data.Add(file);
+
+                        return ++in_nodeIndex;
+                    }
+
+                    default:
+                        throw new NotSupportedException($"Encountered an entry with an unsupported type: {node.Type:X}");
+                }
+            }
         }
 
         public struct Header : IBinarySerializable
@@ -83,15 +146,23 @@ namespace HyperLib.Formats
 
             public void Write(BinaryObjectWriter in_writer)
             {
-                in_writer.Write(_signature);
+                in_writer.Write(Signature);
                 in_writer.Write(NodeTableOffset);
                 in_writer.Write(HeaderSize);
                 in_writer.Write(DataOffset);
 
                 if (IsSonicNextArchive)
                 {
-                    in_writer.Write(_sonicNextCompressedSignature);
-                    in_writer.WriteNullBytes(12);
+                    if (CompressionLevel == CompressionLevel.NoCompression)
+                    {
+                        in_writer.WriteNullBytes(12);
+                        in_writer.Write(_sonicNextUncompressedSignature);
+                    }
+                    else
+                    {
+                        in_writer.Write(_sonicNextCompressedSignature);
+                        in_writer.WriteNullBytes(12);
+                    }
                 }
                 else
                 {
@@ -101,24 +172,26 @@ namespace HyperLib.Formats
             }
         }
 
-        public struct Info : IBinarySerializable
+        public struct Node : IBinarySerializable
         {
             public ENodeType Type;
-            public UInt24 NameOffset;
+            public uint NameOffset;
             public uint DataOffset;
             public uint DataSize;
             public uint UncompressedDataSize;
+
+            public static int SizeOf { get; } = IsSonicNextArchive ? 16 : 12;
 
             public void Read(BinaryObjectReader in_reader)
             {
                 if (in_reader.Endianness == Endianness.Big)
                 {
                     Type = in_reader.Read<ENodeType>();
-                    NameOffset = in_reader.Read<UInt24>();
+                    NameOffset = in_reader.ReadUInt24();
                 }
                 else
                 {
-                    NameOffset = in_reader.Read<UInt24>();
+                    NameOffset = in_reader.ReadUInt24();
                     Type = in_reader.Read<ENodeType>();
                 }
 
@@ -132,12 +205,65 @@ namespace HyperLib.Formats
             public void Write(BinaryObjectWriter in_writer)
             {
                 in_writer.Write(Type);
-                in_writer.Write(NameOffset);
+                in_writer.WriteUInt24(NameOffset);
                 in_writer.Write(DataOffset);
                 in_writer.Write(DataSize);
 
                 if (IsSonicNextArchive)
                     in_writer.Write(UncompressedDataSize);
+            }
+        }
+
+        public class ArchiveNode { }
+
+        public class ArchiveDirectory : ArchiveNode
+        {
+            public string Name { get; set; }
+            public ArchiveNode Parent { get; set; }
+            public List<ArchiveNode> Data { get; set; } = [];
+
+            public ArchiveDirectory() { }
+
+            public ArchiveDirectory(Node in_node, bool in_isRoot = false)
+            {
+                if (in_isRoot)
+                    Name = ".";
+            }
+        }
+
+        public class ArchiveFile : ArchiveNode
+        {
+            public string Name { get; set; }
+            public ArchiveNode Parent { get; set; }
+            public byte[] Data { get; set; }
+            public uint UncompressedDataSize { get; set; }
+            public bool IsCompressed { get; private set; } = IsSonicNextArchive;
+
+            public void Export(string in_path)
+            {
+                File.WriteAllBytes(in_path, IsCompressed ? ZLib.Decompress(Data) : Data);
+            }
+
+            public void Compress(CompressionLevel in_compressionLevel)
+            {
+                if (!IsSonicNextArchive)
+                    throw new NotSupportedException("Compression for this format is only supported by SONIC THE HEDGEHOG.");
+
+                IsCompressed = true;
+
+                UncompressedDataSize = (uint)Data.Length;
+                Data = ZLib.Compress(Data, in_compressionLevel);
+            }
+
+            public void Decompress()
+            {
+                if (!IsSonicNextArchive)
+                    throw new NotSupportedException("Compression for this format is only supported by SONIC THE HEDGEHOG.");
+
+                IsCompressed = false;
+
+                UncompressedDataSize = 0;
+                Data = ZLib.Decompress(Data);
             }
         }
 
